2e750f34f89e281c490f1488321de16fdb96a99a
Atiq Israk <blueniil9@gmail.com>
2022-03-27 17:14:19 +0600

    useGLTF not working

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import { Canvas } from "react-three-fiber";
+import Showcase from "./pages/Showcase";
+
+function App() {
+  return (
+    <>
+      <Showcase />
+    </>
+  );
+}
+
+export default App;
